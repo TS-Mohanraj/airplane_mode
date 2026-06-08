8e737355d7db5338dec6ae8d0e0d44fb8595907d
marimuthu232003 <marimuthu8072620775@gmail.com>
2026-06-08 23:08:04 +0000

    day3 half

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,35 @@
 // Copyright (c) 2026, hemanth balaji and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+    refresh(frm) {
 
-// 	},
-// });
+        frm.add_custom_button("Assign Seat", () => {
+
+            let d = new frappe.ui.Dialog({
+                title: "Assign Seat",
+                fields: [
+                    {
+                        label: "Seat Number",
+                        fieldname: "seat",
+                        fieldtype: "Data",
+                        reqd: 1
+                    }
+                ],
+
+                primary_action(values) {
+
+                    frm.set_value("seat", values.seat);
+
+                    d.hide();
+                },
+
+                primary_action_label: "Assign"
+            });
+
+            d.show();
+
+        });
+
+    }
+});
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,36 @@
 # Copyright (c) 2026, hemanth balaji and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class AirplaneTicket(Document):
-	pass
+
+    def validate(self):
+        self.validate_capacity()
+
+    def validate_capacity(self):
+
+        flight = frappe.get_doc(
+            "Airplane Flight",
+            self.flight
+        )
+
+        airplane = frappe.get_doc(
+            "Airplane",
+            flight.airplane
+        )
+
+        capacity = airplane.capacity
+
+        booked = frappe.db.count(
+            "Airplane Ticket",
+            {
+                "flight": self.flight
+            }
+        )
+
+        if booked >= capacity:
+            frappe.throw(
+                "No seats available"
+            )
