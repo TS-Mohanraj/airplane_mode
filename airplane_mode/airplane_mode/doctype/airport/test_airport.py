1f3e56aa8ce5309fba7d2524c113f42bb2e87d6e
Mohanraj T S <mohanrajts400@gmail.com>
2026-06-08 21:18:19 +0000

    airplane_mode_app : commit

---
+++ airplane_mode/airplane_mode/doctype/airport/test_airport.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, hemanth balaji and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAirport(FrappeTestCase):
+	pass
